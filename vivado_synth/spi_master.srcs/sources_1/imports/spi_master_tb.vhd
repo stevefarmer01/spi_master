ae3f4c413b5eca5e2d6af8457caccac04e4fa00a
Stephen Farmer <SFarmer@ffei.local>
2016-01-25 14:36:06 +0000

    automated tesbench clk speed decreasing of DUT and this shows no failures even when below 1MHz, this is impossible and must be due to rising_edge use in spi_slave on 'i_sclk' which is treating it as a clock. This test bench does not test Slave recieve either

--- vivado_synth/spi_master.srcs/sources_1/imports/spi_master_tb.vhd
+++ vivado_synth/spi_master.srcs/sources_1/imports/spi_master_tb.vhd
@@ -154,6 +154,8 @@ end component;
                                               std_logic_vector(to_unsigned(2#1111111111111110#,DATA_SIZE))
                                               );
 
+    signal input_data_s : input_data_type;
+
     constant period_cycles : delay_type  := ( "00000100", "00001000", "00010000", "00100000");
     constant delay_cycles  : delay_type  := ( "00000111", "00001110", "00011000", "00110000");
     constant four_data     : four_values := ( "00", "10", "01", "11");
@@ -170,9 +172,107 @@ end component;
     signal master_rx_activity : boolean := FALSE;
 
 --.    constant TIME_PERIOD_CLK_DUT : time                          := 16.6 ns;
-    constant TIME_PERIOD_CLK_DUT : time                          := 50 ns;
+    signal TIME_PERIOD_CLK_DUT_S : time                          := 50 ns;
     signal dut_sys_clk_i : std_logic := '0';
-    constant dut_clk_ratio_to_testbench : integer := integer(TIME_PERIOD_CLK_DUT/TIME_PERIOD_CLK);
+--.    constant dut_clk_ratio_to_testbench : integer := integer(TIME_PERIOD_CLK_DUT/TIME_PERIOD_CLK);
+    signal dut_clk_ratio_to_testbench : integer := integer(TIME_PERIOD_CLK_DUT_S/TIME_PERIOD_CLK);
+    signal TIME_PERIOD_CLK_S : time := 10 ns;
+
+procedure spi_main_test_loop (signal TIME_PERIOD_CLK : in time;
+                                signal spi_start_i : out std_logic;
+                                constant FIFO_REQ : in boolean := FALSE;
+                                constant input_data : in input_data_type;
+                                signal data_i : out std_logic_vector(DATA_SIZE - 1 downto 0);
+                                signal wr_i : out std_logic;
+                                signal tx2tx_cycles_i : out std_logic_vector(tx2tx_cycles_i'RANGE);
+                                signal rd_i : out std_logic;
+                                signal stop_clks : out boolean;
+                                signal dut_clk_ratio_to_testbench : integer
+                                 );
+
+procedure spi_main_test_loop (signal TIME_PERIOD_CLK : in time;
+                                signal spi_start_i : out std_logic;
+                                constant FIFO_REQ : in boolean := FALSE;
+                                constant input_data : in input_data_type;
+                                signal data_i : out std_logic_vector(DATA_SIZE - 1 downto 0);
+                                signal wr_i : out std_logic;
+                                signal tx2tx_cycles_i : out std_logic_vector(tx2tx_cycles_i'RANGE);
+                                signal rd_i : out std_logic;
+                                signal stop_clks : out boolean;
+                                signal dut_clk_ratio_to_testbench : integer
+                                 ) is 
+    variable tx2tx_cycles_v : std_logic_vector(tx2tx_cycles_i'LEFT-1 downto 0);
+begin
+
+    --.        wait for 100 * TIME_PERIOD_CLK;
+    --.        if FIFO_REQ = True then
+    --.            wr_i       <= '1';
+    --.            csn_i      <= '0';
+    --.            for i in 0 to 15 loop
+    --.                wait until rising_edge(sys_clk_i);
+    --.                data_i <= input_data(i);
+    --.            end loop;
+    --.            wr_i       <= '0';
+    --.            csn_i      <= '1';
+    --.        end if;
+
+        wait for TIME_PERIOD_CLK* 20 * dut_clk_ratio_to_testbench; -- Wait for sys_rst_i to propagate through DUT especially if DUT is running a much slower clock
+
+        ---for j in 0 to 3 loop
+        for j in input_data_type'RANGE loop
+            ---cpol_i          <= four_data(j)(1);
+            ---cpha_i          <= four_data(j)(0);
+
+            wait until rising_edge(sys_clk_i);
+            spi_start_i     <= '0';
+
+            if FIFO_REQ = False then
+                data_i      <= input_data(j);
+                wr_i        <= '1'; -- write new packet for master to tx
+                wait until rising_edge(sys_clk_i);
+                wr_i        <= '0';
+            end if;
+
+
+            ---for i in 0 to 3 loop
+                ---clk_period_i   <= period_cycles(i);
+                ---setup_cycles_i <= delay_cycles(i);
+                ---hold_cycles_i  <= delay_cycles(i) + 7;
+                tx2tx_cycles_v := std_logic_vector(to_unsigned(16,tx2tx_cycles_i'LENGTH));
+                tx2tx_cycles_i <= tx2tx_cycles_v;
+                ---slave_addr_i   <= four_data(i);
+                ---lsb_first_i    <= four_data(i)(0);
+                ---wait until rising_edge(sys_clk_i);
+
+
+                wait until rising_edge(sys_clk_i);
+                spi_start_i <= '1';
+
+                wait until rising_edge(sys_clk_i);
+                spi_start_i <= '0';
+
+                wait until ss_i = '1'; -- packet has finished when slave select goes low (this ia an active low enable)
+
+                wait until rising_edge(sys_clk_i);
+                rd_i        <= '1';     -- read data rx'd by master
+                wait until rising_edge(sys_clk_i);
+                rd_i        <= '0'; 
+
+                wait for to_integer(unsigned(tx2tx_cycles_v)) * TIME_PERIOD_CLK * dut_clk_ratio_to_testbench; -- wait tx to tx minimum period which is implemented in master's sclk_gen component
+
+            ---end loop;
+        end loop;
+--.        stop_clks <= TRUE;  ----------FINSHED SIMULATION----------.
+        assert not slave_to_master_rx_match_latch = FALSE
+            report "FAIL - Master SPI recieved different to expected" severity Note;
+        assert not (slave_to_master_rx_match_latch = TRUE and master_rx_activity = TRUE)    -- Check for correct data back and that there has actually been some data received
+            report "PASS - Master SPI recieved as expected" severity Note;
+        if slave_to_master_rx_match_latch = FALSE then
+            stop_clks <= TRUE;  ----------FINSHED SIMULATION----------.
+            wait;
+        end if;
+
+end procedure spi_main_test_loop;
 
 begin
 
@@ -191,7 +291,7 @@ end process;
 clk_gen_dut_proc : process
 begin
     while not stop_clks loop
-        wait for TIME_PERIOD_CLK_DUT/2;
+        wait for TIME_PERIOD_CLK_DUT_S/2;
         dut_sys_clk_i <= not dut_sys_clk_i;
     end loop;
     wait;
@@ -332,71 +432,26 @@ end process;
 ss_i <= slave_csn_i(0) and slave_csn_i(1) and slave_csn_i(2) and slave_csn_i(3);
 
 
+TIME_PERIOD_CLK_S <= TIME_PERIOD_CLK;
+
     main_control_proc : process
     begin
-    --.        wait for 100 * TIME_PERIOD_CLK;
-    --.        if FIFO_REQ = True then
-    --.            wr_i       <= '1';
-    --.            csn_i      <= '0';
-    --.            for i in 0 to 15 loop
-    --.                wait until rising_edge(sys_clk_i);
-    --.                data_i <= input_data(i);
-    --.            end loop;
-    --.            wr_i       <= '0';
-    --.            csn_i      <= '1';
-    --.        end if;
+        while TRUE loop
+            TIME_PERIOD_CLK_DUT_S <= TIME_PERIOD_CLK_DUT_S + 1 ns;
+            dut_clk_ratio_to_testbench <= integer(TIME_PERIOD_CLK_DUT_S/TIME_PERIOD_CLK);
+            spi_main_test_loop (TIME_PERIOD_CLK => TIME_PERIOD_CLK_S, -- : in time;
+                                            spi_start_i => spi_start_i, -- : out std_logic;
+                                            FIFO_REQ => FIFO_REQ, -- : in boolean := FALSE;
+                                            input_data => input_data, -- : in input_data_type;
+                                            data_i => data_i, -- : out std_logic_vector(DATA_SIZE - 1 downto 0);
+                                            wr_i => wr_i, -- : out std_logic;
+                                            tx2tx_cycles_i => tx2tx_cycles_i, -- : out std_logic_vector;
+                                            rd_i => rd_i, -- : out std_logic
+                                            stop_clks => stop_clks, -- : out boolean
+                                            dut_clk_ratio_to_testbench => dut_clk_ratio_to_testbench -- : integer
 
-        wait for TIME_PERIOD_CLK* 20 * dut_clk_ratio_to_testbench; -- Wait for sys_rst_i to propagate through DUT especially if DUT is running a much slower clock
-
-        ---for j in 0 to 3 loop
-        for j in input_data_type'RANGE loop
-            ---cpol_i          <= four_data(j)(1);
-            ---cpha_i          <= four_data(j)(0);
-
-            wait until rising_edge(sys_clk_i);
-            spi_start_i     <= '0';
-
-            if FIFO_REQ = False then
-                data_i      <= input_data(j);
-                wr_i        <= '1'; -- write new packet for master to tx
-                wait until rising_edge(sys_clk_i);
-                wr_i        <= '0';
-            end if;
-
-
-            ---for i in 0 to 3 loop
-                ---clk_period_i   <= period_cycles(i);
-                ---setup_cycles_i <= delay_cycles(i);
-                ---hold_cycles_i  <= delay_cycles(i) + 7;
-                tx2tx_cycles_i <= std_logic_vector(to_unsigned(16,tx2tx_cycles_i'LENGTH));
-                ---slave_addr_i   <= four_data(i);
-                ---lsb_first_i    <= four_data(i)(0);
-                ---wait until rising_edge(sys_clk_i);
-
-
-                wait until rising_edge(sys_clk_i);
-                spi_start_i <= '1';
-
-                wait until rising_edge(sys_clk_i);
-                spi_start_i <= '0';
-
-                wait until ss_i = '1'; -- packet has finished when slave select goes low (this ia an active low enable)
-
-                wait until rising_edge(sys_clk_i);
-                rd_i        <= '1';     -- read data rx'd by master
-                wait until rising_edge(sys_clk_i);
-                rd_i        <= '0'; 
-
-                wait for to_integer(unsigned(tx2tx_cycles_i)) * TIME_PERIOD_CLK * dut_clk_ratio_to_testbench; -- wait tx to tx minimum period which is implemented in master's sclk_gen component
-
-            ---end loop;
+                                             ); 
         end loop;
-        stop_clks <= TRUE;  ----------FINSHED SIMULATION----------.
-        assert not slave_to_master_rx_match_latch = FALSE
-            report "FAIL - Master SPI recieved different to expected" severity Note;
-        assert not (slave_to_master_rx_match_latch = TRUE and master_rx_activity = TRUE)    -- Check for correct data back and that there has actually been some data received
-            report "PASS - Master SPI recieved as expected" severity Note;
-        wait;
     end process;
 
 
