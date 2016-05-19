55b67f9436f9f45fbbfd76695d49873eb4005613
Stephen Farmer <SFarmer@ffei.local>
2016-05-19 18:42:02 +0100

    Added comment about future upgrade

--- spi_slave_ip/source/multi_array_types_pkg.vhd
+++ spi_slave_ip/source/multi_array_types_pkg.vhd
@@ -1,14 +1,12 @@
+--Improvements....
+--Get rid of set_data_bit, get_data_bit and replace them with set_data_bits and get_data_bits
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 package multi_array_types_pkg is
 
